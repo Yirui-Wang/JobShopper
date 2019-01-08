b318455d0fff0801e5df395e9c77b2d5cbd1dba0
Yirui Wang <yirui.wyr@gmail.com>
2019-01-08 10:23:53 +0800

    添加了对同一种机器有多台的情况的支持。

--- jobshopper.py
+++ jobshopper.py
@@ -1,3 +1,4 @@
+import time as tm
 from random import sample
 
 import numpy as np
@@ -6,47 +7,69 @@ from deap import base, tools, creator, algorithms
 
 
 class JobShopper:
-    def __init__(self, n_machines):
+    def __init__(self):
         self.__machine_matrix = np.loadtxt('data/machine.csv', dtype='int_', delimiter=',')
         self.__time_matrix = np.loadtxt('data/time.csv', dtype='int_', delimiter=',')
-        self.__n_machines = n_machines
-        self.__n_jobs = self.__machine_matrix.shape[0]
-        self.__n_ops = self.__machine_matrix.shape[1]
-        print('总共有{}个工件，{}台机器，工件最多需要{}道工序。'.format(self.__n_jobs, self.__n_machines, self.__n_ops))
+        self.__n_machines_of_each_type = np.loadtxt('data/n_machine.csv', dtype='int_', delimiter=',')  # 每种机器的数量
+        self.__n_jobs = self.__machine_matrix.shape[0]              # 工件数量
+        self.__n_machine_types = self.__machine_matrix.shape[1]     # 机器种类的数量
+        self.__n_max_ops = self.__machine_matrix.shape[1]           # 工件需要的最大操作数，即机器种类的数量
+        print('总共有{}个工件，{}种机器，一个工件最多需要{}道工序。'.format(self.__n_jobs, self.__n_machine_types, self.__n_max_ops))
 
     def __decode(self, ind):
         seq = [r % self.__n_jobs for r in ind]
         return seq
 
-    def __evaluate(self, ind):
+    def __evaluate(self, ind, save_time_matrix=False):
         """
         适应度的值为所有工件全部加工完成所需要的时间。
+        如果save_time_matrix为True，则将用于绘制甘特图的数据输出到data中。
         """
-        seq = self.__decode(ind)
-        ops = [-1] * self.__n_jobs
-        status = [0] * self.__n_machines
-        start_time = [[-1] * self.__n_ops for _ in range(self.__n_jobs)]
-        end_time = [[-1] * self.__n_ops for _ in range(self.__n_jobs)]
+        seq = self.__decode(ind)    # 随机序列解码后的加工顺序
+        ops = [-1] * self.__n_jobs  # ops[i]代表第i个工件需要加工的工序
+
+        # status为一个list，一个元素为代表一种机器的list，如果该种机器有多台，这个list包含多台机器的状态
+        # 例如有3种机器，数量分别为1，2，3。则status的初始状态为[[0],[0,0],[0,0,0]]
+        # list种的元素n代表该机器直到n时刻这台机器才会空闲
+        status = []
+        for i in range(self.__n_machine_types):
+            status.append([0] * self.__n_machines_of_each_type[i])
+
+        # start_time[i][j]代表第i个工件，第j个工序的开始加工时间，end_time[i][j]同理
+        start_time = [[-1] * self.__n_max_ops for _ in range(self.__n_jobs)]
+        end_time = [[-1] * self.__n_max_ops for _ in range(self.__n_jobs)]
 
         for i in seq:       # 第i个工件
             ops[i] += 1
             j = ops[i]      # 第j道工序
-            machine = self.__machine_matrix[i][j]
+            machine_type = self.__machine_matrix[i][j]
             time = self.__time_matrix[i][j]
 
-            if machine == -1 and time == 0:
+            if machine_type == -1 and time == 0:
                 continue
 
+            least_waiting_machine_time = min(status[machine_type])                              # 该工序将要使用机器的最近空闲时间点
+            least_waiting_machine_num = status[machine_type].index(least_waiting_machine_time)  # 该工序将要使用该种机器的哪一台
+
             if j == 0:      # 如果是第0道工序，则开始时间为该工序将要使用机器的最近空闲时间点
-                start_time[i][j] = status[machine]
+                start_time[i][j] = least_waiting_machine_time
 
             else:           # 如果不是第0道工序，则开始时间为第i个工件第(j-1)道工序的结束时间和该工序将要使用机器的最近空闲时间点之中的较大者
-                start_time[i][j] = end_time[i][j-1] if end_time[i][j-1] > status[machine] else status[machine]
+                start_time[i][j] = end_time[i][j-1] if end_time[i][j-1] > least_waiting_machine_time else least_waiting_machine_time
 
             end_time[i][j] = start_time[i][j] + time
-            status[machine] = end_time[i][j]
+            status[machine_type][least_waiting_machine_num] = end_time[i][j]
 
-        return max(status),
+        if save_time_matrix:
+            timestamp = tm.strftime("%Y_%m_%d_%H_%M_%S", tm.localtime())
+            np.savetxt('data/{}_start_time.csv'.format(timestamp), start_time, fmt='%d', delimiter=',')
+            np.savetxt('data/{}_end_time.csv'.format(timestamp), end_time, fmt='%d', delimiter=',')
+
+        max_time = 0
+        for s in status:
+            max_time = max(s) if max(s) > max_time else max_time
+
+        return max_time,
 
     @staticmethod
     def __crossover(ind1, ind2, indpb=0):
@@ -86,7 +109,7 @@ class JobShopper:
         creator.create("Individual", list, fitness=creator.FitnessMin)
 
         toolbox = base.Toolbox()
-        ind_size = self.__n_jobs * self.__n_ops
+        ind_size = self.__n_jobs * self.__n_max_ops
         toolbox.register("indices", sample, range(ind_size), ind_size)
         toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.indices)
         toolbox.register("population", tools.initRepeat, list, toolbox.individual)
@@ -105,11 +128,10 @@ class JobShopper:
         pop = toolbox.population(n=npop)
         pop, logbook = algorithms.eaSimple(pop, toolbox, cxpb=cxpb, mutpb=mutpb, ngen=ngen, stats=stats, verbose=True)
         JobShopper.plot(logbook)
-        self.get_best(pop)
 
         return pop, logbook
 
-    def get_best(self, pop):
+    def save_best(self, pop):
         min_value = self.__evaluate(pop[0])
         best_ind = pop[0]
 
@@ -119,9 +141,12 @@ class JobShopper:
                 min_value = fitness
                 best_ind = ind
 
+        self.__evaluate(best_ind, save_time_matrix=True)
         best_ind = self.__decode(best_ind)
         print('最少消耗时间{}。'.format(min_value[0]))
         print('最优解为{}。'.format(best_ind))
+
+        return best_ind
 
     @staticmethod
     def plot(logbook):
@@ -132,13 +157,13 @@ class JobShopper:
         fig, ax1 = plt.subplots()
         line1 = ax1.plot(gen, fit_mins, "b-", label="Minimum Fitness")
         ax1.set_xlabel("Generation")
-        ax1.set_ylabel("Fitness", color="b")
+        ax1.set_ylabel("Minimum Fitness", color="b")
         for tl in ax1.get_yticklabels():
             tl.set_color("b")
 
         ax2 = ax1.twinx()
         line2 = ax2.plot(gen, fit_avgs, "r-", label="Average Fitness")
-        ax2.set_ylabel("Size", color="r")
+        ax2.set_ylabel("Average Fitness", color="r")
         for tl in ax2.get_yticklabels():
             tl.set_color("r")
 
@@ -146,45 +171,11 @@ class JobShopper:
         labs = [l.get_label() for l in lns]
         ax1.legend(lns, labs, loc="center right")
 
-        plt.show()
-
-    def save_time_matrix(self, seq):
-        """
-        该函数的输出数据可直接用于绘制甘特图。
-        :param seq: 输入一个加工序列，按照文档中第一种方式编码，即取模后。
-        :return: 输出根据此加工序列加工时，每个工件所有工序的开始加工时间和结束加工时间并保存在data中。
-        """
-        ops = [-1] * self.__n_jobs
-        status = [0] * self.__n_machines
-
-        # start_time[i][j]代表第i个工件，第j个工序的开始加工时间，end_time[i][j]同理
-        start_time = [[-1] * self.__n_ops for _ in range(self.__n_jobs)]
-        end_time = [[-1] * self.__n_ops for _ in range(self.__n_jobs)]
-
-        for i in seq:  # 第i个工件
-            ops[i] += 1
-            j = ops[i]  # 第j道工序
-            machine = self.__machine_matrix[i][j]
-            time = self.__time_matrix[i][j]
-
-            if machine == -1 and time == 0:
-                continue
-
-            if j == 0:  # 如果是第0道工序，则开始时间为该工序将要使用机器的最近空闲时间点
-                start_time[i][j] = status[machine]
-
-            else:  # 如果不是第0道工序，则开始时间为第i个工件第(j-1)道工序的结束时间和该工序将要使用机器的最近空闲时间点之中的较大者
-                start_time[i][j] = end_time[i][j - 1] if end_time[i][j - 1] > status[machine] else status[machine]
-
-            end_time[i][j] = start_time[i][j] + time
-            status[machine] = end_time[i][j]
-
-        np.savetxt('data/start_time.csv', start_time, fmt='%d', delimiter=',')
-        np.savetxt('data/end_time.csv', end_time, fmt='%d', delimiter=',')
-
-        return start_time, end_time
+        timestamp = tm.strftime("%Y_%m_%d_%H_%M_%S", tm.localtime())
+        plt.savefig("data/{}_ga.png".format(timestamp))
 
 
 if __name__ == '__main__':
-    js = JobShopper(n_machines=20)
+    js = JobShopper()
     pop, logbook = js.ga(npop=100, cxpb=0.2, mutpb=0.8, ngen=10, tournsize=50, mu_indpb=0.05)
+    best = js.save_best(pop)
